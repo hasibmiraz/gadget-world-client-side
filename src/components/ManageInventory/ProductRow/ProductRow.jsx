f3b9f6d2cbb8ff26106359d5fef06c91b522d463
hasibmiraz <hasibmiraz09@gmail.com>
2022-05-03 22:54:10 +0600

    Manage inventory design complete

--- src/components/ManageInventory/ProductRow/ProductRow.jsx
+++ src/components/ManageInventory/ProductRow/ProductRow.jsx
@@ -2,36 +2,32 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const ProductRow = ({ product }) => {
-  const { _id, img, name, price, quantity, sold } = product;
+  const { _id, img, name, supplierName, price, quantity, sold } = product;
   const navigate = useNavigate();
   return (
     <tr className="bg-white border-b border-x-2">
-      <td className="flex justify-center px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+      <td className="inventory-img">
         <img
           src={img}
           className="rounded-full w-7 p-1 bg-green-500"
           alt="Avatar"
         />
       </td>
-      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-        {name}
-      </td>
-      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-        {price}
-      </td>
-      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-        {quantity}
-      </td>
-      <td className="text-sm font-bold text-gray-900 px-6 py-4 whitespace-nowrap">
-        {sold}
-      </td>
-      <td className="text-sm font-bold text-gray-900 px-6 py-4 whitespace-nowrap space-x-12">
+      <td className="inventory-td text-left font-light">{name}</td>
+      <td className="inventory-td text-left font-light">{supplierName}</td>
+      <td className="inventory-td font-light">{price}</td>
+      <td className="inventory-td font-light">{quantity}</td>
+      <td className="inventory-td font-bold">{sold}</td>
+      <td className="inventory-td space-x-12 font-light">
         <span
-          className="font-bold text-blue-600"
+          className="font-bold text-blue-600 cursor-pointer"
           onClick={() => navigate(`/inventory/${_id}`)}
         >
           Edit
         </span>
+      </td>
+      <td className="inventory-td space-x-12">
+        <span className="font-bold text-red-600">Delete</span>
       </td>
     </tr>
   );
--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import Title from '../Title/Title';
 
 const Spinner = () => {
   return (
-    <div className="flex justify-center items-center space-x-2 my-[50vh]">
-      <div
-        className="spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0 text-green-600"
-        role="status"
-      >
-        <span className="visually-hidden">Loading...</span>
+    <>
+      <Title title="Loading" />
+      <div className="flex justify-center items-center space-x-2 my-[50vh]">
+        <div
+          className="spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0 text-green-600"
+          role="status"
+        >
+          <span className="visually-hidden">Loading...</span>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
